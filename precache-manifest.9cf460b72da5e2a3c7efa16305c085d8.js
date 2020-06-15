6826b6acaffef85f4363bf1576d4ed4bfb150089
AleksaOpacic <aleksaopacic988@gmail.com>
2020-06-15 15:56:22 +0200

    Updates

---
+++ precache-manifest.9cf460b72da5e2a3c7efa16305c085d8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddcf63a2587896e424ea3130fa9e11d",
+    "url": "/uspPrototip/index.html"
+  },
+  {
+    "revision": "6767535426d32c16cb40",
+    "url": "/uspPrototip/static/css/main.a56131fb.chunk.css"
+  },
+  {
+    "revision": "be87156b05854e1bdd1f",
+    "url": "/uspPrototip/static/js/2.9ed42ccb.chunk.js"
+  },
+  {
+    "revision": "1f05825b069fee380a26611bce4e5a79",
+    "url": "/uspPrototip/static/js/2.9ed42ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6767535426d32c16cb40",
+    "url": "/uspPrototip/static/js/main.2b5c830f.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/uspPrototip/static/js/main.2b5c830f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f012d862aeae56e721e",
+    "url": "/uspPrototip/static/js/runtime-main.697bf3a9.js"
+  },
+  {
+    "revision": "b3de9e808f20c46c813a6c23f7b49a05",
+    "url": "/uspPrototip/static/media/Sound.b3de9e80.mp3"
+  },
+  {
+    "revision": "62c22cdb0326b08c9c4531d584ce3904",
+    "url": "/uspPrototip/static/media/breksva.62c22cdb.jpg"
+  },
+  {
+    "revision": "dfdfeaea5763cf168d2de01bf07b7be8",
+    "url": "/uspPrototip/static/media/cezar.dfdfeaea.jpg"
+  },
+  {
+    "revision": "87af157a5973ffb74839b4a799d7a189",
+    "url": "/uspPrototip/static/media/jaja.87af157a.jpg"
+  },
+  {
+    "revision": "d2d9da902d379120fc44cfe0ecfca244",
+    "url": "/uspPrototip/static/media/map.d2d9da90.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/uspPrototip/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/uspPrototip/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/uspPrototip/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/uspPrototip/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "a7741f81492f3218d603957b3b30adde",
+    "url": "/uspPrototip/static/media/pile.a7741f81.jpg"
+  },
+  {
+    "revision": "d65901e7bc52b13cd0f44507a3ca16aa",
+    "url": "/uspPrototip/static/media/salata.d65901e7.jpg"
+  }
+]);
